473e9ff4d42ab7675e0cb2b15330b7ab7c648770
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-04 17:55:26 -0600

    updated base url

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,7 @@
 
 const api = (function(){
 
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/silas-scottw';
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/scottw';
 
   const getItems = function(){
     return fetch(`${BASE_URL}/items`);
